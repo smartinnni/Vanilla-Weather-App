be0aee768a4e4b6a6cbab0538a8a026dd0e41f5e
smartinnni <101671821+smartinnni@users.noreply.github.com>
2023-09-09 20:17:29 -0500

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-alert("Hello World")
+function displayTemperature(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionELement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round
+    (response.data.temperature.current);
+    cityElement.innerHTML = response.data.city;
+    descriptionELement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+
+let key = "00t0518374baoeb3faaa6c86b66cc02f";
+let apiUrl = 
+    `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${key}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
